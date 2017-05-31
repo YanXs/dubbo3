7621ac1b2805416536eb7fec19cd502b14fc46b5
YanXs <yanss@chinaamc.com>
2017-05-31 17:12:46 +0800

    drop ByteBuddyProxyFactory , udpate AsyncableInvocationHandler JdkProxyFactory

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/proxy/AsyncableInvocationHandler.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/proxy/AsyncableInvocationHandler.java
@@ -4,6 +4,9 @@ import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.async.AsyncCommand;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 
 public class AsyncableInvocationHandler extends InvokerInvocationHandler {
@@ -29,7 +32,9 @@ public class AsyncableInvocationHandler extends InvokerInvocationHandler {
     /**
      * async method wrapper
      */
-    class AsyncMethodCommand extends AsyncCommand<Object> {
+    static class AsyncMethodCommand extends AsyncCommand<Object> {
+
+        static Map<String, Method> cachedMethods = new ConcurrentHashMap<String, Method>(128);
 
         private final Object proxy;
 
@@ -47,7 +52,28 @@ public class AsyncableInvocationHandler extends InvokerInvocationHandler {
             String methodName = wrapped.getName();
             Class<?>[] parameterTypes = wrapped.getParameterTypes();
             String syncMethodName = methodName.substring(methodName.indexOf("async_") + "async_".length());
-            return proxy.getClass().getDeclaredMethod(syncMethodName, parameterTypes);
+            String methodKey = methodKey(syncMethodName, parameterTypes);
+            Method previouslyCached = cachedMethods.get(methodKey);
+            if (previouslyCached != null) {
+                return previouslyCached;
+            }
+            synchronized (AsyncMethodCommand.class) {
+                if (!cachedMethods.containsKey(methodKey)) {
+                    Method method = proxy.getClass().getDeclaredMethod(syncMethodName, parameterTypes);
+                    cachedMethods.put(methodKey, method);
+                }
+            }
+            return cachedMethods.get(methodKey);
+        }
+
+        private String methodKey(String methodName, Class<?>[] parameterTypes) {
+            StringBuilder builder = new StringBuilder(methodName);
+            if (parameterTypes != null && parameterTypes.length > 0) {
+                for (Class<?> clz : parameterTypes) {
+                    builder.append(clz.getName());
+                }
+            }
+            return builder.toString();
         }
 
         @Override
--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/proxy/jdk/JdkProxyFactory.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/proxy/jdk/JdkProxyFactory.java
@@ -23,6 +23,8 @@ import com.alibaba.dubbo.rpc.proxy.AsyncableInvocationHandler;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * JavaassistRpcProxyFactory
@@ -38,15 +40,42 @@ public class JdkProxyFactory extends AbstractProxyFactory {
     }
 
     public <T> Invoker<T> getInvoker(T proxy, Class<T> type, URL url) {
-        return new AbstractProxyInvoker<T>(proxy, type, url) {
-            @Override
-            protected Object doInvoke(T proxy, String methodName,
-                                      Class<?>[] parameterTypes,
-                                      Object[] arguments) throws Throwable {
-                Method method = proxy.getClass().getMethod(methodName, parameterTypes);
-                return method.invoke(proxy, arguments);
-            }
-        };
+        return new CachedProxyInvoker<T>(proxy, type, url);
     }
 
+    static class CachedProxyInvoker<T> extends AbstractProxyInvoker<T> {
+
+        static Map<String, Method> cachedMethods = new ConcurrentHashMap<String, Method>(128);
+
+        public CachedProxyInvoker(T proxy, Class<T> type, URL url) {
+            super(proxy, type, url);
+        }
+
+        @Override
+        protected Object doInvoke(T proxy, String methodName, Class<?>[] parameterTypes, Object[] arguments) throws Throwable {
+            String methodKey = methodKey(methodName, parameterTypes);
+            Method previouslyCached = cachedMethods.get(methodKey);
+            if (previouslyCached != null) {
+                return previouslyCached;
+            }
+            synchronized (CachedProxyInvoker.class) {
+                if (!cachedMethods.containsKey(methodKey)) {
+                    Method method = proxy.getClass().getDeclaredMethod(methodName, parameterTypes);
+                    cachedMethods.put(methodKey, method);
+                }
+            }
+            return cachedMethods.get(methodKey).invoke(proxy, arguments);
+        }
+
+
+        private String methodKey(String methodName, Class<?>[] parameterTypes) {
+            StringBuilder builder = new StringBuilder(methodName);
+            if (parameterTypes != null && parameterTypes.length > 0) {
+                for (Class<?> clz : parameterTypes) {
+                    builder.append(clz.getName());
+                }
+            }
+            return builder.toString();
+        }
+    }
 }
